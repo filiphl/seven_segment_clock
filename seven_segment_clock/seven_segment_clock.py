be844865e638636c76eb8903238f00d680231985
Filip Henrik Larsen <filip.larsen@piscada.com>
2023-02-20 16:34:07 +0100

    Initial commit

---
+++ seven_segment_clock/seven_segment_clock.py
@@ -0,0 +1,128 @@
+def hour(seconds: int, base_24: bool) -> int:
+    """Compute the number of full hours since midnight.
+
+    Parameters
+    ----------
+    seconds : int
+        Number of seconds since midnight.
+    base_24 : bool
+        If True, return hours using a 24-hour based format.
+        If False, return hours using a 12-hour based format.
+
+    Returns
+    -------
+    int
+        Number of full ours since midnight.
+    """
+    if base_24:
+        return int(seconds / 60 / 60)
+    return int(int(seconds / 60 / 60) % 12)
+
+
+def minute(seconds: int) -> int:
+    """Compute the number of minutes the past clock hour.
+
+    Parameters
+    ----------
+    seconds : int
+        Number of seconds since midnight.
+
+    Returns
+    -------
+    int
+        number of minutes the past clock hour
+    """
+
+    return int((seconds - int(seconds / 60 / 60) * 60 * 60) / 60)
+
+
+def lamp(digit: int) -> list[int]:
+    """Get list of segments to light up for a digit.
+
+    Parameters
+    ----------
+    digit : int
+        Digit for which we want a list of segments to light.
+
+    Returns
+    -------
+    list[int]
+        List of segments to light up.
+    """
+    digit_segments = [
+        [1, 1, 0, 1, 1, 1, 1],  # 0
+        [0, 1, 0, 0, 1, 0, 0],  # 1
+        [1, 1, 1, 0, 0, 1, 1],  # 2
+        [1, 1, 1, 0, 1, 1, 0],  # 3
+        [0, 1, 1, 1, 1, 0, 0],  # 4
+        [1, 0, 1, 1, 1, 1, 0],  # 5
+        [1, 0, 1, 1, 1, 1, 1],  # 6
+        [1, 1, 0, 0, 1, 0, 0],  # 7
+        [1, 1, 1, 1, 1, 1, 1],  # 8
+        [1, 1, 1, 1, 1, 1, 0],  # 9
+    ]
+    return digit_segments[digit]
+
+
+def showClock(seconds: int, base24: bool) -> list[list[int]]:
+    """Get list of segment to light up for each digit.
+
+    Parameters
+    ----------
+    seconds : int
+        Number of seconds since midnight.
+    base_24 : bool
+        If True, return hours using a 24-hour based format.
+        If False, return hours using a 12-hour based format.
+
+    Returns
+    -------
+    list[list[int]]
+        List of segments to light up for digits from left to right.
+    """
+    seconds = seconds % (60 * 60 * 24)
+    hour_number = hour(seconds, base24)
+    minute_number = minute(seconds)
+    digits = [
+        int(hour_number / 10),
+        hour_number - int(hour_number / 10) * 10,
+        int(minute_number / 10),
+        minute_number - int(minute_number / 10) * 10,
+    ]
+    return [lamp(digit) for digit in digits]
+
+
+def display_clock(segment_lists):
+    digits = []
+    for segment_list in segment_lists:
+        digit = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+        for index, segment in enumerate(segment_list):
+            if segment == 1:
+                if index == 0:
+                    digit[0][1] = "‾"
+                if index == 1:
+                    digit[0][2] = "|"
+                if index == 2:
+                    digit[1][1] = "―"
+                if index == 3:
+                    digit[0][0] = "|"
+                if index == 4:
+                    digit[2][2] = "|"
+                if index == 5:
+                    digit[2][1] = "_"
+                if index == 6:
+                    digit[2][0] = "|"
+        digits.append(digit)
+        if len(digits) == 2:
+            digits.append([[" ", "·", " "], [" ", " ", " "], [" ", "·", " "]])
+
+    for row in range(3):
+        for digit in digits:
+            print("".join(digit[row]), end="")
+        print()
+
+
+if __name__ == "__main__":
+    clock = showClock(int(60 * 60 * 20), True)
+    # print(clock)
+    display_clock(clock)
